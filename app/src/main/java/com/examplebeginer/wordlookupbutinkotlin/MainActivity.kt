dcf42807d2f7b01c854ceb642459cf2e66e95754
Rafael Herrera <santoElias65537@gmail.com>
2020-05-17 21:59:32 -0600

    ummm yeah

--- app/src/main/java/com/examplebeginer/wordlookupbutinkotlin/MainActivity.kt
+++ app/src/main/java/com/examplebeginer/wordlookupbutinkotlin/MainActivity.kt
@@ -2,11 +2,39 @@ package com.examplebeginer.wordlookupbutinkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchBar = findViewById<TextView>(R.id.searchBar)
+
+        searchBar.setOnClickListener {
+
+
+
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("console.log.alert")
+
+            Toast.makeText(this@MainActivity, "worked", Toast.LENGTH_LONG).show()
+
+            builder.setMessage("fuck")
+
+            builder.setPositiveButton("Ight", null)
+
+
+            //Create the alertDialog
+            val alertDialog: AlertDialog = builder.create()
+
+            //set other dialog properties
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+        }
     }
 }
