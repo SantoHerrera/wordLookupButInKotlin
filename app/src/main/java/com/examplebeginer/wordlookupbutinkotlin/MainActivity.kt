21938ffa733b34abc0aca7da18244e64f5eebda9
Rafael Herrera <santoElias65537@gmail.com>
2020-05-17 21:47:19 -0600

    idk

---
+++ app/src/main/java/com/examplebeginer/wordlookupbutinkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.examplebeginer.wordlookupbutinkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
