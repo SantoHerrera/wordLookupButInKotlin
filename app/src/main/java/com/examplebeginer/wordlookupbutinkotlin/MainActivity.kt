18ee6979a58806129326d2199b52eddf7ad34e96
Rafael Herrera <santoElias65537@gmail.com>
2020-05-25 20:54:24 -0600

    starting to come together

--- app/src/main/java/com/examplebeginer/wordlookupbutinkotlin/MainActivity.kt
+++ app/src/main/java/com/examplebeginer/wordlookupbutinkotlin/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.examplebeginer.wordlookupbutinkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,29 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val searchBar = findViewById<TextView>(R.id.searchBar)
+        val searchButton = findViewById<TextView>(R.id.searchButton)
 
-        searchBar.setOnClickListener {
+        searchButton.setOnClickListener {
 
+            val intent =Intent baseContext, wordDefinitions::class.java)
 
+            startActivity(intent)
 
-            val builder = AlertDialog.Builder(this)
-
-            builder.setTitle("console.log.alert")
-
-            Toast.makeText(this@MainActivity, "worked", Toast.LENGTH_LONG).show()
-
-            builder.setMessage("fuck")
-
-            builder.setPositiveButton("Ight", null)
-
-
-            //Create the alertDialog
-            val alertDialog: AlertDialog = builder.create()
-
-            //set other dialog properties
-            alertDialog.setCancelable(false)
-            alertDialog.show()
         }
+
     }
 }
---
+++ app/src/main/java/com/examplebeginer/wordlookupbutinkotlin/wordDefinitions.kt
@@ -0,0 +1,15 @@
+package com.examplebeginer.wordlookupbutinkotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class wordDefinitions : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
